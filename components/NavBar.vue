ee170b8f96b4d199668c1f625ca0c796982a4c41
Rika <tjj.rikap@gmail.com>
2025-03-20 23:59:04 +0800

    add icon and pwa

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,10 +2,14 @@
   <nav
     class="w-full bg-light dark:bg-dark border-b border-gray-200 dark:border-gray-700 z-50 px-4 py-3 flex justify-between items-center"
   >
+    <NuxtImg src="/icon.svg" alt="logo" class="w-8 h-8" />
     <!-- Title on the left -->
-    <div class="text-2xl font-bold text-gray-800 dark:text-gray-200">
+    <div
+      class="text-2xl font-bold text-gray-800 dark:text-gray-200 px-2 font-serif"
+    >
       你才是输入法
     </div>
+    <div class="flex-1" />
 
     <!-- Controls on the right -->
     <div class="flex items-center gap-3">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,61 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const sw = process.env.SW === "true";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@vueuse/nuxt", "@unocss/nuxt", "@nuxt/eslint"],
+  modules: [
+    "@vueuse/nuxt",
+    "@unocss/nuxt",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@vite-pwa/nuxt",
+  ],
   css: ["@unocss/reset/tailwind-compat.css"],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "128x128",
+          href: "/icon-128.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "512x512",
+          href: "/icon-512.png",
+        },
+      ],
+    },
+  },
+  pwa: {
+    strategies: sw ? "injectManifest" : "generateSW",
+    srcDir: sw ? "service-worker" : undefined,
+    filename: sw ? "sw.ts" : undefined,
+    registerType: "autoUpdate",
+    manifest: {
+      name: "你才是输入法",
+      short_name: "你才是输入法",
+      theme_color: "#f3f3f3",
+      icons: [
+        {
+          src: "icon-128.png",
+          sizes: "128x128",
+          type: "image/png",
+        },
+        {
+          src: "icon-512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "icon-512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+  },
 });
